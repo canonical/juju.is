306d46ddbfbb0ec988e044294fc5033040161c96
Anthony Dillon <me@anthonydillon.com>
2019-10-18 11:53:00 +0100

    Add a tutorials page

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,5 @@
 from canonicalwebteam.flask_base.app import FlaskBase
+from canonicalwebteam.templatefinder import TemplateFinder
 from flask import render_template
 from webapp.docs.views import init_docs
 
@@ -13,9 +14,8 @@ app = FlaskBase(
 )
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
+template_finder_view = TemplateFinder.as_view("template_finder")
+app.add_url_rule("/", view_func=template_finder_view)
+app.add_url_rule("/<path:subpath>", view_func=template_finder_view)
 
 init_docs(app, "/docs")
