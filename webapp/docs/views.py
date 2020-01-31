7e02f62e7e0cdc1a5ef08eb2c8511cb8682d769e
Anthony Dillon <me@anthonydillon.com>
2020-01-31 15:13:45 +0000

    Change the params creating DocsParser

--- webapp/docs/views.py
+++ webapp/docs/views.py
@@ -11,12 +11,14 @@ from canonicalwebteam.search import build_search_view
 
 def init_docs(app, url_prefix):
     discourse_index_id = 1087
+    category_id = 22
 
     discourse_api = DiscourseAPI(base_url="https://discourse.jujucharms.com/")
-    discourse_parser = DocParser(discourse_api, discourse_index_id, url_prefix)
+    discourse_parser = DocParser(
+        discourse_api, category_id, discourse_index_id, url_prefix
+    )
     discourse_docs = DiscourseDocs(
         parser=discourse_parser,
-        category_id=22,
         document_template="docs/document.html",
         url_prefix=url_prefix,
     )
