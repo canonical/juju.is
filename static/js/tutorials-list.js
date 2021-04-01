5cb96e2732b33ea3b5a9e3cf6f81cccc6930b629
Thomas Bille <toto@canonical.com>
2021-04-01 16:27:42 +0100

    Save dropdown state

--- static/js/tutorials-list.js
+++ static/js/tutorials-list.js
@@ -5,6 +5,13 @@
     topicsFilter.addEventListener("change", (e) => {
       window.location.search = `?topic=${e.target.value}`;
     });
+
+    const params = new URLSearchParams(window.location.search);
+    const topicFromUrl = params.get("topic");
+
+    if (topicFromUrl) {
+      topicsFilter.value = topicFromUrl;
+    }
   } else {
     throw new Error(`${selector} is not a valid element!`);
   }
