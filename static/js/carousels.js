59bba4a38a25c5e858fe38713e474116658b8a7f
Scott Mason Nash <sowasred2012@gmail.com>
2021-04-26 16:40:17 +0100

    add testimonial carousel

---
+++ static/js/carousels.js
@@ -0,0 +1,22 @@
+function initCarousel(carouselSelector, nextCTAselector, previousCTAselector) {
+  var nextCTA = document.querySelector(nextCTAselector);
+  var previousCTA = document.querySelector(previousCTAselector);
+  var carousel = new Flickity(carouselSelector, {
+    prevNextButtons: false,
+    wrapAround: true,
+  });
+
+  if (nextCTA) {
+    nextCTA.addEventListener("click", function (e) {
+      e.preventDefault();
+      carousel.next();
+    });
+  }
+
+  if (previousCTA) {
+    previousCTA.addEventListener("click", function (e) {
+      e.preventDefault();
+      carousel.previous();
+    });
+  }
+}
