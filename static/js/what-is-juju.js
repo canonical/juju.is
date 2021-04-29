c84c5516ebacad0f90a7c5b512207493cb6a7273
Anthony Dillon <me@anthonydillon.com>
2021-04-29 11:31:58 +0100

    Build the "What is Juju" animated section of the homepage

---
+++ static/js/what-is-juju.js
@@ -0,0 +1,46 @@
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+let stage = 0;
+const animation = document.querySelector('.p-feature');
+const title = document.querySelector('.p-feature__title');
+
+function setStage(scrollPos) {
+  const cache = stage;
+  const vh = window.innerHeight;
+  const stripPosition = animation.getBoundingClientRect();
+  var bodyRect = document.body.getBoundingClientRect(),
+    elemRect = animation.getBoundingClientRect(),
+    offset   = elemRect.top - bodyRect.top;
+  if (scrollPos >= offset) {
+    scrollPos = scrollPos - offset;
+    stage = Math.floor(scrollPos / vh);
+    half = Math.round(scrollPos / vh);
+    if (stage !== cache) {
+      if (stage < cache) {
+        animation.setAttribute("data-direction", "up");
+      } else {
+        animation.setAttribute("data-direction", "down");
+      }
+    }
+    if (stage !== half) {
+      animation.setAttribute("data-half", "true");
+    } else {
+      animation.setAttribute("data-half", "false");
+    }
+    animation.setAttribute("data-stage", stage);
+  }
+}
+
+document.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      setStage(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
