4329ea96dcc6c7c9e53e075303a47262e1d6881b
Steve Rydz <steve.rydz@canonical.com>
2023-01-09 14:46:13 +0000

    Add charm search

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -48,3 +48,63 @@ function initNavDropdowns(containerClass) {
     });
   });
 }
+
+function initNavigationSearch(element) {
+  const searchButtons = element.querySelectorAll(".js-search-button");
+
+  searchButtons.forEach((searchButton) => {
+    searchButton.addEventListener("click", toggleSearch);
+  });
+
+  const overlay = element.querySelector(".p-navigation__search-overlay");
+  if (overlay) {
+    overlay.addEventListener("click", closeSearch);
+  }
+
+  function toggleSearch(e) {
+    e.preventDefault();
+
+    const navigation = e.target.closest(".p-navigation");
+    if (navigation.classList.contains("has-search-open")) {
+      closeSearch();
+    } else {
+      openSearch(e);
+    }
+  }
+
+  function openSearch(e) {
+    e.preventDefault();
+    const navigation = e.target.closest(".p-navigation");
+    const searchInput = navigation.querySelector(".p-search-box__input");
+    const buttons = document.querySelectorAll(".js-search-button");
+
+    buttons.forEach((searchButton) => {
+      searchButton.setAttribute("aria-pressed", true);
+    });
+
+    navigation.classList.add("has-search-open");
+    searchInput.focus();
+    document.addEventListener("keyup", keyPressHandler);
+  }
+
+  function closeSearch() {
+    const navigation = document.querySelector(".p-navigation");
+    const buttons = document.querySelectorAll(".js-search-button");
+
+    buttons.forEach((searchButton) => {
+      searchButton.removeAttribute("aria-pressed");
+    });
+
+    navigation.classList.remove("has-search-open");
+    document.removeEventListener("keyup", keyPressHandler);
+  }
+
+  function keyPressHandler(e) {
+    if (e.key === "Escape") {
+      closeSearch();
+    }
+  }
+}
+
+const navigation = document.querySelector("#navigation");
+initNavigationSearch(navigation);
